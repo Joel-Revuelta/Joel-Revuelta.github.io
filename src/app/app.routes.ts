6245f3d4c359a3a837c77f8bfda4373b713525bd
Joel Revuelta <joel.revuel@gmail.com>
2025-03-23 14:07:32 +0100

    update build

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,6 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
   },
   {
-    path: 'projects',
-    loadComponent: () => import('./pages/projects/projects.component').then(m => m.ProjectsComponent)
-  },
-  {
     path: '**',
     redirectTo: 'home'
   }
